5370828d463a57692b7688e5ed7188de22368b10
JezCam <jeremy@cameron.org.au>
2024-10-08 16:35:45 +1100

    Added leaderboard

--- src/components/leaderboard.tsx
+++ src/components/leaderboard.tsx
@@ -27,9 +27,9 @@ export default function Leaderboard() {
                     <Trophy01 className="text-foreground w-4 h-4" />
                     <h4>Global Top 10 </h4>
                 </div>
-                <Select>
+                <Select defaultValue="daily">
                     <SelectTrigger className="w-[180px]">
-                        <SelectValue placeholder="Select a fruit" />
+                        <SelectValue />
                     </SelectTrigger>
                     <SelectContent>
                         <SelectGroup>
@@ -77,56 +77,83 @@ export default function Leaderboard() {
                             </div>
                         </div>
                         <div className="flex flex-col w-fit items-center pb-0.5">
-                            <h3 className="leading-4 pb-1 pt-1.5 font-bold">
+                            <h3 className="leading-3 pb-1 pt-1.5 font-bold">
                                 54
                             </h3>
                             <p>Days</p>
                         </div>
                     </div>
-                    {[0, 0].map(() => (
-                        <div className="flex justify-between w-full py-1.5 pl-2 pr-3 mb-3 bg-primary/10 rounded-sm">
-                            <div className="flex gap-6">
-                                <div className="flex items-center gap-1">
-                                    <ChevronUp className="w-5 h-5 text-memoGreen" />
-                                    <div className="text-2xl font-semibold">
-                                        1
-                                    </div>
-                                </div>
-                                <div className="flex gap-3">
-                                    <Avatar className="border-[1px] border-white">
-                                        <AvatarImage
-                                            src="https://github.com/shadcn.png"
-                                            alt="@shadcn"
-                                        />
-                                        <AvatarFallback>JC</AvatarFallback>
-                                    </Avatar>
-                                    <div>
-                                        <h4 className="font-semibold">
-                                            Jeremy Cameron
-                                        </h4>
-                                        <p className="mt-0">2014CAME01</p>
-                                    </div>
-                                </div>
+                    <div className="flex justify-between w-full py-1.5 pl-2 pr-3 mb-3 bg-primary/10 rounded-sm">
+                        <div className="flex gap-6">
+                            <div className="flex items-center gap-1">
+                                <ChevronDown className="w-5 h-5 text-memoRed" />
+                                <div className="text-2xl font-semibold">1</div>
                             </div>
-                            <div className="flex flex-col w-fit items-center pb-0.5">
-                                <h3 className="leading-4 pb-1 pt-1.5 font-bold">
-                                    54
-                                </h3>
-                                <p>Days</p>
+                            <div className="flex gap-3">
+                                <Avatar className="border-[1px] border-white">
+                                    <AvatarImage
+                                        src="https://github.com/shadcn.png"
+                                        alt="@shadcn"
+                                    />
+                                    <AvatarFallback>JC</AvatarFallback>
+                                </Avatar>
+                                <div>
+                                    <h4 className="font-semibold">
+                                        Jeremy Cameron
+                                    </h4>
+                                    <p className="mt-0">2014CAME01</p>
+                                </div>
                             </div>
                         </div>
-                    ))}
+                        <div className="flex flex-col w-fit items-center pb-0.5">
+                            <h3 className="leading-3 pb-1 pt-1.5 font-bold">
+                                54
+                            </h3>
+                            <p>Days</p>
+                        </div>
+                    </div>
+                    <div className="flex justify-between w-full py-1.5 pl-2 pr-3 mb-1 bg-primary/10 rounded-sm">
+                        <div className="flex gap-6">
+                            <div className="flex items-center gap-1">
+                                <div className="flex justify-center w-5">
+                                    <DotsHorizontal className="w-4 h-4 text-primary/60" />
+                                </div>
+                                <div className="text-2xl font-semibold">1</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <Avatar className="border-[1px] border-white">
+                                    <AvatarImage
+                                        src="https://github.com/shadcn.png"
+                                        alt="@shadcn"
+                                    />
+                                    <AvatarFallback>JC</AvatarFallback>
+                                </Avatar>
+                                <div>
+                                    <h4 className="font-semibold">
+                                        Jeremy Cameron
+                                    </h4>
+                                    <p className="mt-0">2014CAME01</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-col w-fit items-center pb-0.5">
+                            <h3 className="leading-3 pb-1 pt-1.5 font-bold">
+                                54
+                            </h3>
+                            <p>Days</p>
+                        </div>
+                    </div>
                     {[3, 4, 5, 6, 7, 8, 9].map((number) => (
-                        <div className="flex justify-between w-full py-1.5 pl-2 pr-3 rounded-sm">
-                            <div className="flex gap-6">
+                        <div className="flex justify-between w-full py-1 pl-2 pr-3 rounded-sm">
+                            <div className="flex gap-7">
                                 <div className="flex items-center gap-2">
                                     <ChevronUp className="w-4 h-4 text-memoGreen" />
                                     <div className="text-xl font-semibold">
                                         {number}
                                     </div>
                                 </div>
-                                <div className="flex gap-3">
-                                    <Avatar className="border-[1px] border-white">
+                                <div className="flex gap-3 items-center">
+                                    <Avatar className="border-[1px] border-white w-8 h-8">
                                         <AvatarImage
                                             src="https://github.com/shadcn.png"
                                             alt="@shadcn"
@@ -142,7 +169,7 @@ export default function Leaderboard() {
                                 </div>
                             </div>
                             <div className="flex flex-col w-fit items-center pb-0.5">
-                                <h3 className="leading-4 pb-1 pt-1.5 font-bold">
+                                <h3 className="leading-4 pb-1 pt-1.5 font-bold text-xl">
                                     54
                                 </h3>
                                 <p>Days</p>
